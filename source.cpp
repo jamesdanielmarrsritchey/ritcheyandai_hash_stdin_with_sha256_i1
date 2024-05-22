739c3bd00b3896b5e0c9827860f0b79a1c42242c
user1 <75754139+jamesdanielmarrsritchey@users.noreply.github.com>
2024-05-22 15:15:30 -0700

    v1

--- source.cpp
+++ source.cpp
@@ -3,8 +3,9 @@
 #include <cryptopp/hex.h>
 #include <iostream>
 #include <string>
+#include <getopt.h>
 
-int main() {
+int main(int argc, char* argv[]) {
     std::string line;
     if (!std::getline(std::cin, line) || line.empty()) {
         return 1;
@@ -13,10 +14,32 @@ int main() {
     CryptoPP::SHA256 hash;
     std::string digest;
 
+    bool useUppercase = false;
+
+    struct option long_options[] = {
+        {"letter_case", required_argument, 0, 'l'},
+        {0, 0, 0, 0}
+    };
+
+    int opt;
+    int option_index = 0;
+    while ((opt = getopt_long(argc, argv, "l:", long_options, &option_index)) != -1) {
+        if (opt == 'l') {
+            if (std::string(optarg) == "upper") {
+                useUppercase = true;
+            } else if (std::string(optarg) == "lower") {
+                useUppercase = false;
+            } else {
+                std::cerr << "Invalid argument for --letter_case: " << optarg << std::endl;
+                return 1;
+            }
+        }
+    }
+
     CryptoPP::StringSource s(line, true, 
         new CryptoPP::HashFilter(hash, 
             new CryptoPP::HexEncoder(
-                new CryptoPP::StringSink(digest))));
+                new CryptoPP::StringSink(digest), useUppercase)));
 
     std::cout << digest << std::endl;
 
